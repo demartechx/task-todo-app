0cc96720ae82ec9a901d104d73169cf5d99511de
Martex1230#$ <adeagbothompson@gmail.com>
2023-06-06 20:41:50 +0100

    add

--- lib/app/modules/home/view.dart
+++ lib/app/modules/home/view.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:task_todo_app/app/core/utils/extensions.dart';
+import 'package:task_todo_app/app/data/models/task.dart';
 import 'package:task_todo_app/app/modules/home/controller.dart';
 import 'package:task_todo_app/app/modules/home/widgets/add_card.dart';
+import 'package:task_todo_app/app/modules/home/widgets/task_card.dart';
 
 class HomePage extends GetView<HomeController> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(4.0.wp),
-            child: Text('My List',
-            style: TextStyle(fontSize: 24.0.sp, fontWeight: FontWeight.bold),),
+        body: SafeArea(
+            child: ListView(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(4.0.wp),
+          child: Text(
+            'My List',
+            style: TextStyle(fontSize: 24.0.sp, fontWeight: FontWeight.bold),
           ),
-          GridView.count(crossAxisCount: 2, shrinkWrap: true, physics: const ClampingScrollPhysics(),
-          children: [
-            AddCard()
-          ],
-          )
-        ],
-      ))
-    );
+        ),
+        Obx(() => GridView.count(
+            crossAxisCount: 2,
+            shrinkWrap: true,
+            physics: const ClampingScrollPhysics(),
+            children: [
+              // TaskCard(
+              //   task: Task(
+              //     title: 'title',
+              //     icon: 0xe59c,
+              //     color: '#FF2B60E6',
+              //   ),
+              // ),
+              ...controller.tasks.map((element) => TaskCard(
+                task: element,
+              ),).toList(),
+              AddCard()
+            ],
+          ),
+        )
+      ],
+    )));
   }
 }
--- lib/app/modules/home/widgets/add_card.dart
+++ lib/app/modules/home/widgets/add_card.dart
@@ -1,7 +1,9 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:task_todo_app/app/core/utils/extensions.dart';
+import 'package:task_todo_app/app/data/models/task.dart';
 import 'package:task_todo_app/app/modules/home/controller.dart';
 import 'package:task_todo_app/app/widgets/icon.dart';
 
@@ -20,71 +22,91 @@ class AddCard extends StatelessWidget {
       child: InkWell(
         onTap: () async {
           await Get.defaultDialog(
-              titlePadding: EdgeInsets.symmetric(vertical: 5.0.wp),
-              radius: 5,
-              title: 'Task type',
-              content: Form(
-                  key: homeCtrl.formKey,
-                  child: Column(children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 3.0.wp),
-                      child: TextFormField(
-                        controller: homeCtrl.editCtrl,
-                        decoration: const InputDecoration(
-                            border: OutlineInputBorder(), labelText: 'Text'),
-                        validator: (value) {
-                          if (value == null || value.trim().isEmpty) {
-                            return 'Please enter your task title';
-                          }
-                          return null;
-                        },
-                      ),
+            titlePadding: EdgeInsets.symmetric(vertical: 5.0.wp),
+            radius: 5,
+            title: 'Task type',
+            content: Form(
+              key: homeCtrl.formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 3.0.wp),
+                    child: TextFormField(
+                      controller: homeCtrl.editCtrl,
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(), labelText: 'Text'),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter your task title';
+                        }
+                        return null;
+                      },
                     ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 5.0.wp),
-                      child: Wrap(
-                          spacing: 2.0.wp,
-                          children: icons
-                              .map((e) => Obx(() {
-                                    final index = icons.indexOf(e);
-                                    return ChoiceChip(
-                                      selectedColor: Colors.grey.shade200,
-                                      pressElevation: 0,
-                                      backgroundColor: Colors.white,
-                                      label: e,
-                                      selected:
-                                          homeCtrl.chipIndex.value == index,
-                                      onSelected: (bool selected) {
-                                        homeCtrl.chipIndex.value == selected
-                                            ? index
-                                            : 0;
-                                      },
-                                    );
-                                  }))
-                              .toList()),
-                    ),
-                    ElevatedButton(
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 5.0.wp),
+                    child: Wrap(
+                        spacing: 2.0.wp,
+                        children: icons
+                            .map(
+                              (e) => Obx(
+                                () {
+                                  final index = icons.indexOf(e);
+                                  return ChoiceChip(
+                                    selectedColor: Colors.grey.shade200,
+                                    pressElevation: 0,
+                                    backgroundColor: Colors.white,
+                                    label: e,
+                                    selected: homeCtrl.chipIndex.value == index,
+                                    onSelected: (bool selected) {
+                                      homeCtrl.chipIndex.value == selected
+                                          ? index
+                                          : 0;
+                                    },
+                                  );
+                                },
+                              ),
+                            )
+                            .toList()),
+                  ),
+                  ElevatedButton(
                       style: ElevatedButton.styleFrom(
-                        primary: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-
-                        ),
-                        minimumSize: const Size(150, 40)
-                      ),
-                      onPressed: () { }, 
-                      child: const Text('Confirm')
-                      )
-                  ]
-                  )
-                  ));
+                          primary: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          minimumSize: const Size(150, 40)),
+                      onPressed: () {
+                        if (homeCtrl.formKey.currentState!.validate()) {
+                          int icon =
+                              icons[homeCtrl.chipIndex.value].icon!.codePoint;
+                          String color =
+                              icons[homeCtrl.chipIndex.value].color!.toHex();
+                          var task = Task(
+                              title: homeCtrl.editCtrl.text,
+                              icon: icon,
+                              color: color);
+                          Get.back();
+                          homeCtrl.addTask(task)
+                              ? EasyLoading.showSuccess('create success')
+                              : EasyLoading.showError('Duplicated Task');
+                        }
+                      },
+                      child: const Text('Confirm'))
+                ],
+              ),
+            ),
+          );
+          homeCtrl.editCtrl.clear();
+          homeCtrl.changeChipIndex(0);
         },
         child: DottedBorder(
-            color: Colors.grey.shade400,
-            dashPattern: const [8, 4],
-            child: Center(
-              child: Icon(Icons.add, size: 19.0.wp, color: Colors.grey),
-            )),
+          color: Colors.grey.shade400,
+          dashPattern: const [8, 4],
+          child: Center(
+            child: Icon(Icons.add, size: 19.0.wp, color: Colors.grey),
+          ),
+        ),
       ),
     );
   }
